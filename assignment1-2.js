a3a416c42a5326e8daeff96f334a08e609833d85
NadiaAhmed15 <nadiaahmedoo15@gmail.com>
2024-09-25 14:47:52 +0300

    Add files via upload

---
+++ assignment1-2.js
@@ -0,0 +1,6 @@
+function addJavaScriptPrefix(str) {
+    return  str +"JavaScript ";
+}
+
+var String = "welcome to ";
+console.log(addJavaScriptPrefix(String));
---
+++ assignment1.js
@@ -0,0 +1,7 @@
+var side1 = 5 ;
+var side2 = 6 ;
+var side3 = 7 ;
+
+var r =(side1+side2+side3)/2;
+var area =  Math.sqrt(r*((r-side1)*(r-side2)*(r-side3)));
+console.log(area);
